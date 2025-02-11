ed55bbb03e9e81569113085cfbacfb6deccea196
Lukas K Schumann <lukas_kilian.schumann@stud-mail.uni-wuerzburg.de>
2025-02-11 23:29:38 +0100

    added support for timestamps (%s)

--- lua/dateformatter/init.lua
+++ lua/dateformatter/init.lua
@@ -6,14 +6,19 @@ local P = function (v)
    return v
 end
 
+M.date_input_format = "%d-%m-%Y"
+
 M.dump_text = function ()
    local mode = vim.api.nvim_get_mode().mode
    local vstart = vim.fn.getcharpos(".")  -- bufnum, lnum, col, off
    local vend = vim.fn.getcharpos("v")  -- bufnum, lnum, col, off
 
    if mode == "V" then
+      -- FIXME: line selection is currently broken. requires triming of whitespace,etc if there are less characters then maxcol
       vstart[3]=0
-      vend[3]=64 --TODO: FIXME: this should be maxcol
+      vend[3]=vim.v.maxcol
+      -- print(tostring(vim.v.maxcol))
+      -- vim.api.nvim_set_current_line(tostring(vim.v.maxcol))
    end
    local line_min = 0
    local line_max = 0
@@ -26,7 +31,7 @@ M.dump_text = function ()
    end
    local ret = vim.api.nvim_buf_get_lines(vstart[1],line_min-1,line_max, true)
 
-   local format_str = vim.fn.input("Date format: ", "%d-%m-%Y")
+   local format_str = vim.fn.input("Date input format: ", M.date_input_format)
    if format_str == "" then
       vim.print("canceled")
       return
@@ -42,12 +47,13 @@ M.dump_text = function ()
          col_min = vstart[3]
          col_max = vend[3]
       end
-      -- TODO: handle shorter strings! (are already handled implicitly, but still?)
+      -- shorter stings are handled implicitely
       local line_left = ""
       if col_min > 1 then
         line_left = string.sub(key,1,col_min-1)
       end
       local line_middle = string.sub(key,col_min,col_max)
+      -- catch python exceptions from remote plugin
       if pcall(function() line_middle = vim.fn.FormatDate(line_middle, format_str) end) then
       else
          line_middle = "ERROR"
@@ -61,8 +67,33 @@ M.dump_text = function ()
 end
 
 
+M.set_default_output_format = function ()
+   local format_str = vim.fn.input("Date output format: ", "%Y-%m-%d")
+   if format_str == "" then
+      vim.print("canceled")
+      return
+   end
+   vim.fn.FormatDateSetFormat(format_str)
+end
+M.set_default_intput_format = function ()
+   local format_str = vim.fn.input("Date input format: ", M.date_input_format)
+   if format_str == "" then
+      vim.print("canceled")
+      return
+   end
+   M.date_input_format = format_str
+end
 
 
+vim.keymap.set({'n', 'v'}, 'mo', M.set_default_output_format, {noremap = true, silent = false})
+vim.keymap.set({'n', 'v'}, 'mi', M.set_default_intput_format, {noremap = true, silent = false})
 vim.keymap.set({'n', 'v'}, 'mx', M.dump_text, {noremap = true, silent = false})
 
 return M
+
+-- TODO: config/setup function to configure some defaults
+
+
+
+
+
--- rplugin/python3/remoteplug.py
+++ rplugin/python3/remoteplug.py
@@ -6,8 +6,14 @@ from datetime import datetime
 class Limit(object):
     def __init__(self, vim):
         self.vim = vim
-        self.calls = 0
-        self.defaultformat = "_%Y-%m-%d_"
+        self.defaultformat = "%Y-%m-%dT%H:%M:%S"
+
+
+    @pynvim.function('FormatDateSetFormat', sync=True)
+    def set_format(self, args):
+        assert(len(args)==1)
+        new_format = args[0]
+        self.defaultformat = new_format
 
     """
     Expects 2 parameters: a date as string and a format string.
@@ -18,5 +24,9 @@ class Limit(object):
         assert(len(args)==2)
         inp = args[0]
         format_str = args[1]
-        dateobj = datetime.strptime(inp, format_str)
+
+        if format_str == "%s":
+            dateobj = datetime.fromtimestamp(float(inp))
+        else:
+            dateobj = datetime.strptime(inp, format_str)
         return dateobj.strftime(self.defaultformat)
